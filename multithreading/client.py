8c41e70c3148487d0e2054d1b02d2e8264ff098f
elixez9 <a1381ahmadi@gmail.com>
2025-10-14 18:34:58 +0330

    multithreading

---
+++ multithreading/client.py
@@ -0,0 +1,28 @@
+import socket
+
+server_ip = "127.0.0.1"
+server_port = 8080
+
+
+def run_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+
+    try:
+        while True:
+            msg = input("enter your message: ")
+            client_socket.send(msg.encode("utf-8")[:1024])
+
+            response = client_socket.recv(1024)
+            response = response.decode("utf-8")
+
+            if response.lower() == "closed":
+                break
+
+            print(f"Received: {response}")
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+    finally:
+        client_socket.close()
+        print("Client disconnected")
