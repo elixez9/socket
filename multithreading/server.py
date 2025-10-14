56ffe3ea6030c7e0572cbaf2f977bdf84e2ed107
elixez9 <a1381ahmadi@gmail.com>
2025-10-14 18:53:12 +0330

    add threading

---
+++ multithreading/server.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+server_ip = "127.0.0.1"
+server_port = 8080
+
+def handle_client(client_socket, client_address):
+    try:
+        pass
+
+
+def run_server():
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((server_ip, server_port))
+        server_socket.listen(0)  #چک کن back log
+        print(f"Server listening on {server_ip}:{server_port}")
+
+        while True:
+             client_socket, client_address = server_socket.accept()
+             print(f"Accepted connection from {client_address[0]} : {client_address[1]}")
+             ##Using the threading module and automatically creating it to connect multiple clients
+             thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+             thread.start()
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+    finally:
+        server_socket.close()
+        print("Client disconnected")
